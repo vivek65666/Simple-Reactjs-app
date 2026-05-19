72cc9b1b84bd0738c29f00df43a353d5733c88f7
Vivek <vivekcraj321@gmail.com>
2026-05-19 11:54:10 +0530

    Switch to HashRouter for GitHub Pages compatibility

--- src/App.js
+++ src/App.js
@@ -1,29 +1,29 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Customers from './Customers'
-import { BrowserRouter as Router, Switch, Route, Redirect} from 'react-router-dom';
-
+import Customers from './Customers';
+// CHANGED: Swapped BrowserRouter out for HashRouter
+import { HashRouter as Router, Switch, Route, Redirect} from 'react-router-dom';
 
 class App extends Component {
   render() {
     console.log("Host URL"+process.env.PUBLIC_URL);
     return (
-
-      <Router basename={process.env.PUBLIC_URL}>
+      // REMOVED: basename is no longer required when using HashRouter
+      <Router>
         <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Simple React App</h1>
-        </header>
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Simple React App</h1>
+          </header>
           <Switch>
-                <Route exact path= "/" render={() => (
-                  <Redirect to="/customerlist"/>
-                )}/>
-                 <Route exact path='/customerlist' component={Customers} />
+            <Route exact path="/" render={() => (
+              <Redirect to="/customerlist"/>
+            )}/>
+            <Route exact path='/customerlist' component={Customers} />
           </Switch>
-      </div>
-    </Router>
+        </div>
+      </Router>
     );
   }
 }
